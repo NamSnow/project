a9ff67ea0cdc804a9ff549e8da517e7cf0b77b88
NamSnow <phamhoainam08102004@gmail.com>
2025-07-21 10:24:23 +0700

    1

--- components/layout/header/index.vue
+++ components/layout/header/index.vue
@@ -6,7 +6,7 @@
         style="background-image: url('/images/hero-bg.jpg')"
       ></div>
       <div
-        class="absolute inset-0 bg-[#0a2c0f] opacity-70 z-10 w-full h-full"
+        class="absolute inset-0 bg-[#0a2c0f] opacity-80 z-10 w-full h-full"
       ></div>
     </div>
 
@@ -250,21 +250,14 @@
             </li>
           </ul>
 
-          <a
-            href=""
-            class="text-white h-[50px] items-center lg:flex hidden pl-7.5"
-          >
-            <button
-              class="px-7.5 cursor-pointer bg-[#d1a132] rounded-full h-full hover:bg-white hover:text-black font-bold"
-            >
-              Shop Now
-            </button>
-            <button
-              class="bg-[#FFFFFF1A] w-[50px] h-full rounded-full cursor-pointer transform -rotate-45 hover:rotate-0"
-            >
-              <i class="fa-solid fa-arrow-right"></i>
-            </button>
-          </a>
+          <div class="pl-7.5">
+            <Button
+              text="Shop Now"
+              link="/"
+              hoverTextColor="black"
+              hoverBackgroundColor="white"
+            />
+          </div>
 
           <button
             class="bg-[#d1a132] w-9.5 h-9.5 rounded-lg cursor-pointer focus:bg-white lg:hidden text-white focus:text-black"
@@ -277,15 +270,18 @@
     </div>
 
     <div
-      class="relative z-20 flex lg:flex-row flex-col text-white max-w-[1300px] mx-auto p-2.5 mt-20 pb-20 items-center flex-1"
+      class="relative z-20 flex lg:flex-row flex-col text-white max-w-[1300px] mx-auto p-5 mt-20 pb-20 items-center flex-1"
     >
-      <div class="lg:w-1/2 flex flex-col gap-4 w-full">
+      <div class="lg:w-1/2 flex flex-col gap-8.5 w-full mb-5">
         <div class="flex items-center">
           <div class="w-2 h-2 mr-4 bg-[#D1A132] rounded-full"></div>
-          <div class="text-lg">Discover The Power Of Premium</div>
+          <div class="text-lg font-normal italic">
+            Discover The Power Of Premium
+          </div>
         </div>
-        <div class="text-6xl">
-          Pure drops perfect precision <span>care you can trust</span>.
+        <div class="lg:text-6xl md:text-5xl text-4xl font-semibold">
+          Pure drops perfect precision
+          <span class="font-normal italic font-lora">care you can trust</span>.
         </div>
         <p class="text-base">
           Experience the perfect balance of purity and precision with our
@@ -293,21 +289,15 @@
           high-quality droppers ensure accurate dispensing, minimal waste, and
           maximum potency.
         </p>
-        <div class="flex gap-10">
-          <a href="" class="text-white h-[50px] items-center lg:flex hidden">
-            <button
-              class="px-7.5 cursor-pointer bg-[#d1a132] rounded-full h-full hover:bg-white hover:text-black font-bold"
-            >
-              Purchase Now
-            </button>
-            <button
-              class="bg-[#FFFFFF1A] w-[50px] h-full rounded-full cursor-pointer transform -rotate-45 hover:rotate-0"
-            >
-              <i class="fa-solid fa-arrow-right"></i>
-            </button>
-          </a>
+        <div class="flex gap-10 flex-col sm:flex-row">
+          <Button
+            text="Purchase Now"
+            link="/"
+            hoverTextColor="black"
+            hoverBackgroundColor="white"
+          />
 
-          <div class="flex gap-4">
+          <div class="flex gap-4 group">
             <div
               class="bg-[#D1A132] w-[50px] flex items-center justify-center rounded-full"
             >
@@ -316,7 +306,7 @@
 
             <div>
               <div class="font-bold">Call Us</div>
-              <div class="hover:text-[#D1A132] cursor-pointer">
+              <div class="group-hover:text-[#D1A132] cursor-pointer">
                 +91 - 123 468 963
               </div>
             </div>
@@ -327,53 +317,7 @@
       <div class="relative lg:mr-[55px] lg:ml-[75px]">
         <img src="/public/images/hero-image.png" alt="" />
         <div class="absolute top-0 left-0">
-          <svg
-            width="200"
-            height="200"
-            viewBox="0 0 200 200"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <defs>
-              <path
-                id="circlePath"
-                d="M 100, 100 m -80, 0 a 80,80 0 1,1 160,0 a 80,80 0 1,1 -160,0"
-              />
-              <clipPath id="clip0_461_42709">
-                <rect
-                  width="60"
-                  height="60"
-                  fill="white"
-                  transform="translate(0 0.535156)"
-                />
-              </clipPath>
-            </defs>
-
-            <g transform="translate(70, 70)">
-              <g clip-path="url(#clip0_461_42709)">
-                <path
-                  d="M31.3211 0.541016C34.504 3.71338 37.3005 7.41271 38.653 11.7479C40.3639 16.7295 39.5489 22.3786 36.8824 26.8755C35.3998 29.6052 33.1725 31.8079 31.3457 34.2917C28.9813 37.6081 27.2002 41.6552 27.6569 45.8113C27.8747 48.7729 29.5223 51.5342 31.8726 53.3118C34.2932 55.1492 37.4269 55.9853 40.4447 55.6832C42.4612 55.4478 44.3724 54.682 46.1254 53.6772C44.2073 55.8764 41.5654 57.4503 38.6811 57.9667C34.5461 58.6975 30.0774 58.1389 26.4659 55.908C20.9362 52.69 17.7463 46.1907 17.9396 39.8741C17.8763 36.1186 18.853 32.4052 20.3636 28.9869C17.4196 32.76 15.6806 37.5168 15.8457 42.3298C16.06 48.0913 18.7019 53.7966 23.1671 57.4925C24.6883 58.7572 26.4378 59.6917 28.2225 60.5278C23.2163 60.0184 18.2803 57.9527 14.7848 54.2674C10.2528 49.5984 7.99739 42.8497 8.69651 36.3961C9.29022 31.035 12.2272 26.2361 15.8949 22.4032C18.2698 19.8351 21.1049 17.7519 23.5149 15.2189C26.1498 12.4541 28.5528 9.38707 30.0283 5.8388C30.7133 4.149 31.3773 2.39594 31.3211 0.541016Z"
-                  fill="#0B3311"
-                />
-                <path
-                  d="M42.5173 19.1422C42.8053 16.9184 42.3451 14.7051 42.2538 12.4883C46.1112 18.3763 49.5716 24.7316 50.7766 31.7508C51.7076 36.6973 51.9711 42.2094 49.2906 46.6851C46.6874 51.333 40.6518 53.6025 35.621 51.9268C32.684 50.915 30.5902 47.9183 30.6604 44.8126C30.4953 40.3791 33.2356 36.5603 35.9723 33.3387C39.4327 29.3654 42.0606 24.454 42.5173 19.1422Z"
-                  fill="#D1A132"
-                />
-              </g>
-            </g>
-
-            <text
-              font-family="Arial"
-              font-size="16"
-              fill="#FFFFFF"
-              text-anchor="middle"
-              letter-spacing="4.8"
-            >
-              <textPath href="#circlePath" startOffset="50%">
-                PREMIUM QUALITY * PREMIUM QUALITY *
-              </textPath>
-            </text>
-          </svg>
+          <img src="/public/icons/icon2.svg" alt="" />
         </div>
       </div>
     </div>
@@ -381,7 +325,10 @@
 
   <!--  -->
   <div
-    :class="{ 'translate-x-0': isMenuOpen, 'translate-x-full': !isMenuOpen }"
+    :class="{
+      'm-0 hidden': !isMenuOpen,
+      'translate-x-full': !isMenuOpen,
+    }"
     class="bg-[#d1a132] fixed top-0 left-0 w-screen h-screen z-[100] transform transition-transform duration-300 ease-in-out lg:hidden"
   >
     <div
@@ -449,6 +396,8 @@
 </template>
 
 <script setup>
+import Button from "~/components/Button.vue";
+
 const isMenuOpen = ref(false);
 
 const toggleMenu = () => {
